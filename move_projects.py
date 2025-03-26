53c004059485b2ad5dfcee8a0bd21b71d357b466
Edmond Sabou <edmond.sabou@snyk.io>
2025-03-26 10:20:53 +0200

    added python construct

--- move_projects.py
+++ move_projects.py
@@ -1,38 +1,29 @@
 import requests
 import os
 
-# Ask for user input
-current_org = input("Enter the org ID where your current projects are: ")
-move_org = input("Enter the org ID where you would like to move your projects: ")
-auth_token = os.getenv("SNYK_TOKEN")
-
 API_VERSION = "version=2024-10-15"
 
-
-# Get all projects enpoint for an Organisation
-def get_all_projects():
+# Get all projects endpoint for an Organisation
+def get_all_projects(current_org, auth_token):
     url = f"https://api.snyk.io/rest/orgs/{current_org}/projects?&{API_VERSION}"
     
     response = requests.get(url, headers={"Authorization": f"token {auth_token}"})
 
     if response.status_code == 200:
         data = response.json().get("data", [])
-
         project_info = [{"id": project["id"], "name": project["attributes"]["name"]} for project in data]
         return project_info
     else:
         print(f"❌ Failed to get projects: {response.status_code} - {response.text}")
         return []
 
-
-
 # Move projects endpoint
-def move_project(project_id):
+def move_project(current_org, move_org, auth_token, project_id):
     url = f"https://api.snyk.io/v1/org/{current_org}/project/{project_id}/move"    
     
     response = requests.put(
         url,
-        headers={"Content-Type":"application/json", "Authorization": f"token {auth_token}"},
+        headers={"Content-Type": "application/json", "Authorization": f"token {auth_token}"},
         json={"targetOrgId": move_org}
     )
 
@@ -43,19 +34,22 @@ def move_project(project_id):
         print(f"❌ Failed to move project ID: {project_id}. Status Code: {response.status_code}")
         return None
 
-# Get projects and move them to the new organization
-projects = get_all_projects()
+if __name__ == "__main__":
+    # Ask for user input
+    current_org = input("Enter the org ID where your current projects are: ")
+    move_org = input("Enter the org ID where you would like to move your projects: ")
+    auth_token = os.getenv("SNYK_TOKEN")
 
+    # Get projects and move them to the new organization
+    projects = get_all_projects(current_org, auth_token)
 
-# Print the retrieved projects
-print()
-print(f"🎯 Retrieved {len(projects)} projects.")
-for project in projects:
-    print(f"{project['name']}: {project['id']}")
-
-
-
-for project in projects:
+    # Print the retrieved projects
     print()
-    print(f"⏳ Moving Project - ID: {project['id']}, Name: {project['name']}")
-    move_project(project['id'])
+    print(f"🎯 Retrieved {len(projects)} projects.")
+    for project in projects:
+        print(f"{project['name']}: {project['id']}")
+
+    for project in projects:
+        print()
+        print(f"⏳ Moving Project - ID: {project['id']}, Name: {project['name']}")
+        move_project(current_org, move_org, auth_token, project['id'])
